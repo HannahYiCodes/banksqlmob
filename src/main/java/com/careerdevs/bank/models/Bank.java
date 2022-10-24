371eb8210edd93a714580c3817bad5212878f206
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-10-24 19:49:49 -0400

    Bank Mob Code

---
+++ src/main/java/com/careerdevs/bank/models/Bank.java
@@ -0,0 +1,63 @@
+package com.careerdevs.bank.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Bank {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private String phoneNumber;
+
+    public Bank(String name, String location, String phoneNumber) {
+        this.name = name;
+        this.location = location;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // default constructor
+    public Bank() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAreaCode() {
+        return phoneNumber.substring(0, 3);
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
